c30c1bbcfa1d0d2828bc5d08277e05a690dc76ca
Tim Sawicki <tsawicki@redhat.com>
2022-10-14 15:26:58 -0400

    add more config options; still need to confirm oauth setup

--- fluvii/auth/config.py
+++ fluvii/auth/config.py
@@ -1,9 +1,11 @@
 from fluvii.config_base import KafkaConfigBase
+import requests
+import time
 
 
 class SaslPlainClientConfig(KafkaConfigBase):
     def __init__(self, username, password):
-        self.security_protocol = 'sasl_ssl'
+        self.security_protocol = 'SASL_SSL'
         self.mechanism = 'PLAIN'
         self.username = username
         self.password = password
@@ -14,4 +16,32 @@ class SaslPlainClientConfig(KafkaConfigBase):
             "sasl.mechanism": self.mechanism,
             "sasl.username": self.username,
             "sasl.password": self.password,
+        }
+
+
+class SaslOauthClientConfig(KafkaConfigBase):
+    def __init__(self, username, password, url, scope):
+        self.security_protocol = 'sasl_ssl'
+        self.mechanism = 'OUATHBEARER'
+        self.username = username
+        self.password = password
+        self.url = url
+        self.scope = scope
+
+    def _get_token(self):
+        payload = {
+            'grant_type': 'client_credentials',
+            'scope': ' '.join(self.scope)
+        }
+        resp = requests.post(self.url,
+                             auth=(self.username, self.password),
+                             data=payload)
+        token = resp.json()
+        return token['access_token'], time.time() + float(token['expires_in'])
+
+    def as_client_dict(self):
+        return {
+            'security.protocol': 'SASL_SSL',
+            'sasl.mechanism': 'OAUTHBEARER',
+            'oauth_cb': self._get_token,
         }
--- fluvii/fluvii_app/config.py
+++ fluvii/fluvii_app/config.py
@@ -15,6 +15,8 @@ class FluviiConfig:
     """
     def __init__(self, client_urls=None, schema_registry_url=None,
                  client_auth_config=None, schema_registry_auth_config=None, producer_config=None, consumer_config=None):
+        self._time = int(datetime.timestamp(datetime.now()))
+
         # Required vars
         if not client_urls:
             client_urls = environ['FLUVII_KAFKA_BOOTSTRAP_SERVERS']
@@ -41,9 +43,31 @@ class FluviiConfig:
         self.consumer_config = consumer_config
         self.producer_config = producer_config
         self.app_name = environ.get("FLUVII_APP_NAME", 'fluvii_app')
-        self.hostname = environ.get('FLUVII_HOSTNAME', f'{self.app_name}_{int(datetime.timestamp(datetime.now()))}')
         self.table_folder_path = environ.get('FLUVII_TABLE_FOLDER_PATH', '/tmp')
-        self.table_changelog_topic = environ.get('FLUVII_TABLE_CHANGELOG_TOPIC', f'{self.app_name}__changelog')
         self.table_recovery_multiplier = int(environ.get('FLUVII_TABLE_RECOVERY_MULTIPLIER', '10'))
         self.loglevel = environ.get('FLUVII_LOGLEVEL', 'INFO')
         self.enable_metrics_pushing = True if environ.get('FLUVII_ENABLE_METRICS_PUSHING', 'false').lower() == 'true' else False
+
+        self._hostname = None
+        self._table_changelog_topic = None
+
+        # These properties allow settings that depend on others to dynamically adjust their defaults
+        @property
+        def hostname(self):
+            if self._hostname:
+                return self._hostname
+            return environ.get('FLUVII_HOSTNAME', f'{self.app_name}_{self._time}')
+
+        @hostname.setter
+        def hostname(self, value):
+            self._hostname = value
+
+        @property
+        def table_changelog_topic(self):
+            if self._table_changelog_topic:
+                return self._table_changelog_topic
+            return environ.get('FLUVII_TABLE_CHANGELOG_TOPIC', f'{self.app_name}__changelog')
+
+        @table_changelog_topic.setter
+        def table_changelog_topic(self, value):
+            self._table_changelog_topic = value
